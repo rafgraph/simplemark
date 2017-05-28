4621e703ea5c882de4fb4b21b33e513e62d9349a
rafrex <code@rafrex.com>
2017-05-27 22:47:57 -0700

    Add key to props

--- src/stackWork.js
+++ src/stackWork.js
@@ -7,6 +7,8 @@ export function emptyStack(stack, renderer) {
 export function closeNode(stack, renderer) {
   const node = stack.pop();
   node.props.children = node.children;
+  const parent = stack[stack.length - 1];
+  node.props.key = parent.children.length;
   const element = renderer[node.type](node.props);
-  stack[stack.length - 1].children.push(element);
+  parent.children.push(element);
 }
